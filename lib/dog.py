81349ccdc885a5828dce93843688525776cc9d7b
Mwenda <kelvinfotech@gmail.com>
2024-05-27 10:17:44 +0300

    Python P3 Init Self

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
+# Define a Dog class in lib/dog.py
 class Dog:
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
     pass
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
+# Define a Person class in lib/person.py
 class Person:
+    def __init__(self, name):
+        self.name = name
+
     pass
